f03ae202cb5994b38846252e0ce332c76b808724
Irawan Firmansyah <irawanfrmnsyh@gmail.com>
2019-03-23 20:18:15 +0700

    second commit

---
+++ src/main/java/com/project/propensib8/repository/SurveiDB.java
@@ -0,0 +1,13 @@
+package com.project.propensib8.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.propensib8.model.SurveiModel;
+
+@Repository
+public interface SurveiDB extends JpaRepository<SurveiModel, Long>{
+	
+}
